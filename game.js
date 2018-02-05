8c0a2ef75291cd434f7a1a87e24dd3aecc6b21aa
Joe Davis <joecrush@msn.com>
2018-02-05 09:22:29 -0700

    third commit

--- game.js
+++ game.js
@@ -1,33 +1,27 @@
-var health = 100
-var hits = 0
+var player = new Player("Fresh Meat", 100, "assets/images/FreshMeat.jpg", 0)
 
-var players = [];
-var enemies = [];
-
-function PlayerMaker(name, health, img) {
+function Player(name, health, img) {
     this.name = name
     this.health = health
-    //this.attacks = ["slap", "punch", "kick"]
     this.img = img
+    this.hits = 0
 }
 
-var player1 = new PlayerMaker("Fresh Meat", 100, "assets/images/FreshMeat.jpg")
-var player2 = new PlayerMaker("Pew Pew", 100, "assets/images/SpaceDefender.jpg")
-
-players.push(player1, player2)
-
-function drawPlayer(arr) {
+function drawPlayer() {
+    if(player.health <= 0){
+        // <img src="${player.img}" alt="target"> // need to figure out how to die
+    }
     var template = ''
     var playerElem = document.getElementById("player-card")
-    for (let i = 0; i < arr.length; i++) {
-        var player = arr[i];
-        template += `
-        <div class="card">
+    template += `
+        <div class="card align-self-center centerFlex text-center card-custom p-6">
             <div class="card-header">
-                <h4> ${player.name} Health: ${player.health}
+                <h4> ${player.name}<br>
+                    Health: ${player.health}
                 </h4>
-                <p>Hits:<span id="player.hits">0</span>
+                <p>Hits:<span id="player.hits"> ${player.hits}</span>
                 </p>
+                <h4 id="youDied">...</h4>
             </div>
             <div class="card-body">
                 <img src="${player.img}" alt="target">
@@ -36,100 +30,46 @@ function drawPlayer(arr) {
                 <button type="button" class="btn btn-danger" onClick="slap()">Slap!</button>
                 <button type="button" class="btn btn-warning" onClick="punch()">Punch!</button>
                 <button type="button" class="btn btn-dark" onClick="kick()">Kick!</button>
+                <div class="row" >
+                        <div class="col-sm-4 d-sm-inline-flex" text-align="center">
+
+                                <img src="assets/images/Shield-Mod.jpg" alt="Shield Mod">
+
+                                <img src="assets/images/Elixir-Mod.jpg" alt="Elixir Mod">
+
+                                <img src="assets/images/Love-Mod.jpg" alt="Love Mod">
+
+                        </div>
+                    </div>            
+                </div>
             </div>
-        </div>
-          
+        </div> 
         `
 
-    }
     playerElem.innerHTML = template
 }
 
-
-function EnemyMaker(name, health, img) {
-    this.name = name
-    this.health = health
-    this.img = img
+function slap() {
+    player.health -= 1
+    player.hits += 1
+    drawPlayer()
 }
-
-var enemy1 = new EnemyMaker("Draggin", 80, "assets/images/EvilDragon.jpg")
-var enemy2 = new EnemyMaker("Chompy", 80, "assets/images/SpaceInvader.jpg")
-
-enemies.push(enemy1, enemy2)
-
-function drawEnemy(arr) {
-    var template = ''
-    var enemyElem = document.getElementById("enemy-card")
-    for (let i = 0; i < arr.length; i++) {
-        var enemy = arr[i];
-        template += `
-        <div class="card">
-            <div class="card-header">
-                <h4> ${enemy.name} Health: ${enemy.health}
-                </h4>
-                <p>Hits:<span id="hits">0</span>
-                </p>
-            </div>
-            <div class="card-body">
-                <img src="${enemy.img}" alt="target">
-            </div>
-            <div class="card-footer">
-                <button type="button" class="btn btn-danger" onClick="slap()">Counter1!</button>
-                <button type="button" class="btn btn-warning" onClick="punch()">Counter2!</button>
-                <button type="button" class="btn btn-dark" onClick="kick()">Counter3!</button>
-            </div>
-        </div>
-          
-        `
-
-    }
-    enemyElem.innerHTML = template
-}
-
-
-//write  a button to enter player name and start game
-
-// function startGame() {
-//     health = 100
-//     hits = 0
-//     player = prompt("Player1! Enter your Name!")
-//     onStart()
-// }
-
-function onStart() {
-    health = 100
-    hits = 0
-    document.getElementById("playerName").innerHTML = player1.name
-    document.getElementById("health").innerHTML = player.health
-    document.getElementById("hits").innerHTML = hits
-}
-
-
-function onDamage() {
-    document.getElementById("player.health").innerHTML = player.health
-    document.getElementById("player.hits").innerHTML = player.hits
-}
-
-function slap(index) {
-    var players = player[index]
-    health -= 1
-    hits += 1
-    onDamage()
-    drawPlayer(players)
-}
-
 function punch() {
-    health -= 5
-    hits += 1
-    onDamage()
+    player.health -= 5
+    player.hits += 1
+    drawPlayer()
 }
 
 function kick() {
-    health -= 10
-    hits += 1
-    onDamage()
+    player.health -= 10
+    player.hits += 1
+    drawPlayer()
 }
 
+// var items ={        //need to figure out how to have a dictionary of items that decrease damage
+//     shield: new Items("Shield", 0.3, "Shield yourself from danger"),
+//     elixir: new Items("Elixir", 1.6, "An elixir for what ails you"),
+//     love: new Items("Love", 4.0, "Behold the power of love")
+// }
 
-drawPlayer(players)
-drawEnemy(enemies)
+drawPlayer()
