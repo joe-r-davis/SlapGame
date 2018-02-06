87953ecec88525372d043dbe76e0fd4709b53174
Joe Davis <joecrush@msn.com>
2018-02-05 17:37:16 -0700

    fourth commit

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
-var player = new Player("Fresh Meat", 100, "assets/images/FreshMeat.jpg", 0)
+var Item = function(name, damage, itemBlurb){
+    this.name = name;
+    this.damage = damage;
+    this.itemBlurb = itemBlurb;
+}
 
-function Player(name, health, img) {
+//Step 3 - create the items Dictionary
+var items ={        //need to figure out how to have a dictionary of items that decrease damage
+    shield: new Item("shield", 0.3, "Shield yourself from danger"),
+    elixir: new Item("Elixir", 1.6, "An elixir for what ails you"),
+    love: new Item("Love", 4.0, "Behold the power of love")
+}
+
+var player = new Player("Fresh Meat", 100, "assets/images/FreshMeat.jpg", 0,"")  //Step 4- Give some items to our target
+
+function Player(name, health, img, hits, dead) {
     this.name = name
     this.health = health
     this.img = img
     this.hits = 0
+    this.dead = ""
+    this.items= [items.shield, items.elixir, items.love] //Step 4- Give some items to our target
 }
 
 function drawPlayer() {
     if(player.health <= 0){
-        // <img src="${player.img}" alt="target"> // need to figure out how to die
+        player.dead = "You Died!"
     }
     var template = ''
     var playerElem = document.getElementById("player-card")
@@ -21,7 +36,7 @@ function drawPlayer() {
                 </h4>
                 <p>Hits:<span id="player.hits"> ${player.hits}</span>
                 </p>
-                <h4 id="youDied">...</h4>
+                <h4>${player.dead}</h4>
             </div>
             <div class="card-body">
                 <img src="${player.img}" alt="target">
@@ -65,11 +80,21 @@ function kick() {
     player.hits += 1
     drawPlayer()
 }
+//Step 2 - Items
+//Step 5 reduce the damage
 
-// var items ={        //need to figure out how to have a dictionary of items that decrease damage
-//     shield: new Items("Shield", 0.3, "Shield yourself from danger"),
-//     elixir: new Items("Elixir", 1.6, "An elixir for what ails you"),
-//     love: new Items("Love", 4.0, "Behold the power of love")
-// }
+function addMods(toAdd){
+    var itemDamage = 0
+    for (let i = 0; i < player.items.length; i++) {
+        const item = player.items[i]; //ALIAS
+        itemDamage += item.damage
+        
+    }
+    
+    return itemDamage
+}
+
 
 drawPlayer()
+debugger
+addMods("shield", "elixir")
